be87a8ed154fb70abadd5d6df26114e1a249baf5
Hoang Cuong <hoangcuongbk80@gmail.com>
2020-10-16 08:35:37 +0200

    addmove franka

--- grasp_detection/src/grasp_detection_node.cpp
+++ grasp_detection/src/grasp_detection_node.cpp
@@ -3,6 +3,11 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/PointCloud2.h>
 
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+
+#include <tf_conversions/tf_eigen.h>
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -11,7 +16,7 @@
 #include <pcl/point_types.h>
 #include <pcl/conversions.h>
 #include <pcl/common/transforms.h>
-
+#include <pcl/io/ply_io.h>
 
 using namespace cv;
 using namespace std;
@@ -28,11 +33,18 @@ class readrgbdNode
     void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
     void cloudPublish();
 
+    void depthToClould(cv::Mat &depth_img);
+
     bool image_save;
     std::string depth_topsub, rgb_topsub, cloud_topsub, cloud_toppub;
-    std::string saved_rgb_dir, saved_depth_dir;
+    std::string saved_rgb_dir, saved_depth_dir, saved_points_dir;
+    std::string optical_frame_id;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr scene_cloud;
+    double fx, fy, cx, cy, depth_factor;
+    double table_height;
 
+    tf::TransformListener tf_listener;
 
   private:
    ros::NodeHandle nh_, nh_rgb, nh_depth, nh_cloud;
@@ -55,14 +67,27 @@ readrgbdNode::readrgbdNode()
   nh_.getParam("image_save", image_save);
   nh_.getParam("saved_rgb_dir", saved_rgb_dir);
   nh_.getParam("saved_depth_dir", saved_depth_dir);
+  nh_.getParam("saved_points_dir", saved_points_dir);
+
+  nh_.getParam("table_height", table_height);
+  nh_.getParam("optical_frame_id", optical_frame_id);
+
+
+  nh_.getParam("fx", fx);
+  nh_.getParam("fy", fy);
+  nh_.getParam("cx", cx);
+  nh_.getParam("cy", cy);
+  nh_.getParam("depth_factor", depth_factor);
 
   std::cerr << "depth topics sub: " << "\n" << depth_topsub << "\n";
   std::cerr << "rgb topics sub: " << "\n" << rgb_topsub << "\n";
   std::cerr << "cloud topics sub: " << "\n" << cloud_topsub << "\n";
   std::cerr << "save depth to: " << "\n" << saved_depth_dir << "\n";
   std::cerr << "save rgb to: " << "\n" << saved_rgb_dir << "\n";
+  std::cerr << "save points to: " << "\n" << saved_points_dir << "\n";
 
-  cloud_ .reset(new pcl::PointCloud<pcl::PointXYZ>);
+  cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+  scene_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
 
 }
 
@@ -80,6 +105,57 @@ void readrgbdNode::subcribeTopics()
 void readrgbdNode::advertiseTopics()
 {
   cloud_pub = nh_.advertise<sensor_msgs::PointCloud2> (cloud_toppub, 1);
+}
+
+void readrgbdNode::depthToClould(cv::Mat &depth_img)
+{
+   scene_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
+   pcl::PointXYZRGB point;
+   for(int row=0; row < depth_img.rows; row++)
+    {
+       for(int col=0; col < depth_img.cols; col++)       
+        {
+          if(isnan(depth_img.at<ushort>(row, col))) continue;
+          double depth = depth_img.at<ushort>(row, col) / depth_factor;
+          point.x = (col-cx) * depth / fx;
+          point.y = (row-cy) * depth / fy;
+          point.z = depth;
+          point.b = 190;//rgb_img.at<cv::Vec3b>(row, col)[0];
+          point.g = 190;//rgb_img.at<cv::Vec3b>(row, col)[1];
+          point.r = 190;//rgb_img.at<cv::Vec3b>(row, col)[2];
+         scene_cloud->push_back(point);
+        }
+    }
+
+  // Publish
+  pcl::PCLPointCloud2 cloud_filtered;
+  sensor_msgs::PointCloud2 output;
+  scene_cloud->header.frame_id = optical_frame_id;
+  pcl::toPCLPointCloud2(*scene_cloud, cloud_filtered);
+  pcl_conversions::fromPCL(cloud_filtered, output);
+  cloud_pub.publish(output);
+
+  //convert to world
+  tf::StampedTransform transform;
+  Eigen::Affine3d Tcam_offset;
+  try
+  {
+      tf_listener.lookupTransform(optical_frame_id, "world", ros::Time(0), transform);
+      tf::poseTFToEigen(transform, Tcam_offset);
+      Eigen::Matrix4d m = Tcam_offset.matrix().inverse();
+      pcl::transformPointCloud (*scene_cloud, *scene_cloud, m);
+  }
+  catch (tf::TransformException ex)
+  {
+      ROS_ERROR("%s",ex.what());
+  }
+
+  // Save pointcloud for votegrasp
+  Eigen::Affine3f transform_pc = Eigen::Affine3f::Identity();
+  transform_pc.translation() << 0, 0, -table_height;
+  pcl::transformPointCloud (*scene_cloud, *scene_cloud, transform_pc);
+  pcl::io::savePLYFileBinary(saved_points_dir, *scene_cloud);
+  std::cerr << "save: " << saved_points_dir << "\n";
 }
 
 void readrgbdNode::depthCallback (const sensor_msgs::Image::ConstPtr& msg)
@@ -100,8 +176,9 @@ void readrgbdNode::depthCallback (const sensor_msgs::Image::ConstPtr& msg)
   depth_img = bridge->image.clone();
   depth_img.convertTo(depth_img, CV_16UC1, 1000.0);
   //cv::imshow("depth", depth_img);
+  //cv::waitKey(3);
   if(image_save) cv::imwrite( saved_depth_dir, depth_img );
-  cv::waitKey(3);
+  depthToClould(depth_img);
 }
 
 void readrgbdNode::rgbCallback (const sensor_msgs::Image::ConstPtr& msg)
@@ -119,8 +196,8 @@ void readrgbdNode::rgbCallback (const sensor_msgs::Image::ConstPtr& msg)
   cv::Mat rgb_image;
   rgb_image = bridge->image;
   //cv::imshow("RGB image", rgb_image);
+  //cv::waitKey(3);
   if(image_save) cv::imwrite( saved_rgb_dir, rgb_image );
-  cv::waitKey(3);
 }
 
 void readrgbdNode::cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
@@ -129,22 +206,8 @@ void readrgbdNode::cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_m
   pcl_conversions::toPCL(*cloud_msg, *cloud2);
   
   cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>);
-  cloud_->header.frame_id = "camera_link";
   pcl::fromPCLPointCloud2( *cloud2, *cloud_);
-  //Eigen::Affine3f transform_pc = Eigen::Affine3f::Identity();
-  //transform_pc.translation() << 0.045, 0.85, -1.31;
-  //pcl::transformPointCloud (*cloud_, *cloud_, transform_pc);
-  //transform_pc = Eigen::Affine3f::Identity();
-  //transform_pc.rotate (Eigen::AngleAxisf (-0.745, Eigen::Vector3f::UnitY()));
-  //transform_pc.rotate (Eigen::AngleAxisf (-1.56, Eigen::Vector3f::UnitZ()));
-  //pcl::transformPointCloud (*cloud_, *cloud_, transform_pc);
-
-  // do something on cloud_ then  
-  pcl::toPCLPointCloud2(*cloud_, *cloud2);
-
-  sensor_msgs::PointCloud2 output;  
-  pcl_conversions::fromPCL(*cloud2, output);
-  cloud_pub.publish(output);
+  // do something
 }
 
 int main(int argc, char** argv)
--- naf_env/src/scripting.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-clear
-python main.py --num_episodes 50000 --batch_size 32
-python main.py --num_episodes 50000 --batch_size 512
-python main.py --num_episodes 50000 --batch_size 256
